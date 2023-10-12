c1a3c6f0d7722f1bfacb53aafdc3b3f23bebfee4
Lim Rocky <lim.rocky@cashwalk.io>
2023-10-12 11:41:37 +0900

    init commit

---
+++ VoiceMemo/App/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by Rocky on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
